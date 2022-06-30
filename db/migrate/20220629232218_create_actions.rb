66d4e9957a3238a0cf599f91678f1190ed435cf6
Brandon <brandonkanute@gmail.com>
2022-06-30 03:00:15 +0300

    Created table rows

--- db/migrate/20220629232218_create_actions.rb
+++ db/migrate/20220629232218_create_actions.rb
@@ -1,4 +1,13 @@
 class CreateActions < ActiveRecord::Migration[6.1]
   def change
+    create_table :actions do |t|
+      t.string :name
+      t.string :title
+      t.string :original_title
+      t.string :original_name
+      t.string :overview
+      t.string :backdrop_path
+      t.string :poster_path
+    end
   end
 end
