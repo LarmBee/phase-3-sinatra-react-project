0dfb5db29fe78cfc55b7c34c47aa5de8532d8767
Brandon <brandonkanute@gmail.com>
2022-06-30 03:05:27 +0300

    Create tables

--- db/migrate/20220629232655_create_netflix_originals.rb
+++ db/migrate/20220629232655_create_netflix_originals.rb
@@ -1,6 +1,6 @@
 class CreateNetflixOriginals < ActiveRecord::Migration[6.1]
   def change
-    create table :netflix_originals do |t|
+    create_table :netflix_originals do |t|
       t.string :name
       t.string :poster_path
       t.string :backdrop_path
